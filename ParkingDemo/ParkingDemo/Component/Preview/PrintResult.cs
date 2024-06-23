aeb52b3a56c3f446216f30aa09ddc7ac1002e62d
FatemehImanizadeh <94775381+FatemehImanizadeh@users.noreply.github.com>
2024-06-23 17:11:14 +0330

    homeRemote

---
+++ ParkingDemo/ParkingDemo/Component/Preview/PrintResult.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Grasshopper.Kernel;
+using ParkingDemo.Utils;
+using Rhino.Geometry;
+
+namespace ParkingDemo.components.Preview
+{
+    
+
+    public class PrintResult : GH_Component
+    {
+        List<int> existingIndices = new ParkingPreview().ExistingIndices;
+       
+        public PrintResult()
+          : base("PrintResult", "PrintResult",
+                  "PrintResult",
+              "ParkingDemo", "Preview")
+        {
+        }
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddRectangleParameter("Outline BoundingRectangle", "BRec", "outline bounding rectangle",
+                GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Reset Preview sheet", "Reset Preview", "reset preview size for export image results: if the plan area changes set the reset value to true to create a new layout of your plan ", GH_ParamAccess.item);
+            
+        }
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            if (existingIndices.Count == 0) existingIndices.Add(0);
+            var index = existingIndices.Max () + 1 ;
+            existingIndices.Add(index);
+            var resetPlan = false; 
+            var bRec = new Rectangle3d(Plane.Unset, 10, 10);
+            DA.GetData(0, ref bRec);
+            DA.GetData(1, ref resetPlan); 
+            ParkingPreview.PrintResult("testname", index, bRec,  resetPlan); 
+        }
+
+        protected override System.Drawing.Bitmap Icon => null;
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("F21AEDDE-4A45-4165-A23A-16FE59852FA0"); }
+        }
+    }
+}
+
---
+++ ParkingDemo/ParkingDemo/Utils/ParkingPreview.cs
@@ -0,0 +1,127 @@
+﻿using Rhino.Commands;
+using Rhino;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grasshopper.Kernel.Geometry.Voronoi;
+using Rhino.Display;
+using System.IO;
+using Grasshopper.Kernel.Parameters.Hints;
+using Rhino.Geometry;
+using Rhino.Render;
+namespace ParkingDemo.Utils
+{
+    internal class ParkingPreview
+    {
+        public List<int> ExistingIndices = new List<int>() ;
+
+        public static void PrintResult(string DocumentName, int Index, Rectangle3d PlanArea, bool resetPlan)
+        {
+            // Set up export options
+            var doc = RhinoDoc.ActiveDoc;
+            RhinoPageView viewRectangle;
+            try
+            {
+                viewRectangle = doc.Views.GetPageViews()[0];
+               var detail2 =  viewRectangle.GetDetailViews()[0];
+               doc.Objects.Delete(detail2.Id, true);
+            }
+            catch
+            {
+                viewRectangle = doc.Views.AddPageView($"{DocumentName}_{Index}", PlanArea.Width, PlanArea.Height);
+            }
+            var width = (int)PlanArea.Width;
+            var height = (int)PlanArea.Height;
+            viewRectangle.PageWidth = PlanArea.Width;
+            viewRectangle.PageHeight = PlanArea.Height;
+            var corner0 = new Point2d(PlanArea.Corner(3).X, PlanArea.Corner(3).Y);
+            var corner1 = new Point2d(PlanArea.Corner(1).X, PlanArea.Corner(1).Y);
+            viewRectangle.AddDetailView(DocumentName, corner0, corner1, DefinedViewportProjection.Top);
+            var detail = viewRectangle.GetDetailViews()[0];
+                detail.Viewport.SetCameraLocation(PlanArea.Center, true);
+            var vec = new Vector3d(-PlanArea.Corner(0));
+            detail.Geometry.Transform(Transform.Translation(vec));
+            detail.CommitChanges();
+            detail.CommitViewportChanges();
+            //RhinoViewport.FromId(viewRectangle.ActiveViewportID).ZoomExtents();
+            int W = (int)viewRectangle.GetDetailViews()[0].DetailGeometry.GetBoundingBox(true).Max.X -
+                    (int)viewRectangle.GetDetailViews()[0].DetailGeometry.GetBoundingBox(true).Min.X;
+            int H = (int)viewRectangle.GetDetailViews()[0].DetailGeometry.GetBoundingBox(true).Max.Y -
+                    (int)viewRectangle.GetDetailViews()[0].DetailGeometry.GetBoundingBox(true).Min.Y;
+            
+
+
+
+            var view_capture = new ViewCapture
+            {
+                Width = width*13,
+                Height = height*13,
+                ScaleScreenItems = true,
+                DrawAxes = false,
+                DrawGrid = false,
+                DrawGridAxes = false,
+                TransparentBackground = true,
+            };
+            view_capture.TransparentBackground = true;
+            var bitmapnew = view_capture.CaptureToBitmap(viewRectangle);
+               Size size = new Size(width * 10, height * 10);
+            var sizeNew = new Size(width * 10, height * 10);
+               var image2 =
+                   viewRectangle.GetPreviewImage(size, false);
+            
+            image2.SetResolution(2000*width, 2000*height);
+            image2.MakeTransparent();
+            // image.Save(fileName);
+            if (null != image2)
+            {
+                string filename2;
+                try
+                {
+                    filename2 = $"G:\\archtech\\thesis\\FatemeThesis\\Visualization\\imcap\\imcap6\\{DocumentName}_{Index.ToString()}.png";
+                }
+                catch
+                {
+                    filename2 = $"D:\\FatemeThesis\\Visualization\\imcap\\imcap6\\{DocumentName}_{Index.ToString()}.png";
+                }
+                // Change filename and format here
+             //   var path = $"C:\\ParkingDemo\\Result";
+              //  var filename = Path.Combine(path, "SampleCsViewCapture.png");
+                //bitmapnew.Save(filename2, System.Drawing.Imaging.ImageFormat.Jpeg);
+                image2.Save(filename2);
+
+            }
+            doc.Objects.Delete(viewRectangle.ActiveDetailId, true);
+            /*
+            var size = view.Size;
+            var bitmapOptions = new Rhino.Display.ViewCaptureSettings(view, 300); // 300 dpi
+            // bitmapOptions.SetModelScaleToFit(true);
+            bitmapOptions.SetLayout(Size.Subtract(Size.Empty, Size.Empty), view.ScreenRectangle );
+            bitmapOptions.DrawBackground = false;
+            bitmapOptions.DrawWallpaper = false;
+            // Capture viewport to bitmap
+            var bitmap = view.CaptureToBitmap();
+            if (bitmap == null)
+            {
+                RhinoApp.WriteLine("Failed to capture viewport to bitmap.");
+            }
+            // Export bitmap to a special format
+            try
+            {
+                string filename = $"C:\\ParkingDemo\\Result\\{DocumentName}_{Index.ToString()}.png"; // Change filename and format here
+                bitmap.Save(filename, System.Drawing.Imaging.ImageFormat.Png); // Change ImageFormat to desired special format
+                RhinoApp.WriteLine("Export successful. File saved to: " + filename);
+            }
+            catch (Exception ex)
+            {
+                RhinoApp.WriteLine("Error exporting file: " + ex.Message);
+            }
+            finally
+            {
+                bitmap.Dispose();
+            }*/
+        }
+    }
+}
