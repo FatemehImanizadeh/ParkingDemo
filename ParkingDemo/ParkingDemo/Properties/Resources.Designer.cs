12a91030e85513e6f70f0669c8a37a68ff5318c3
varen <varen@gmail.com>
2026-08-16 19:49:01 +0330

    #10 : preview and icons update

--- ParkingDemo/ParkingDemo/Properties/Resources.Designer.cs
+++ ParkingDemo/ParkingDemo/Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace ParkingDemo.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ParkingCollection {
+            get {
+                object obj = ResourceManager.GetObject("ParkingCollection", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ParkingSolver {
             get {
                 object obj = ResourceManager.GetObject("ParkingSolver", resourceCulture);
